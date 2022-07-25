92fb48c833fde66693aa6556f0ca470accadc55c
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-07-25 13:09:40 +0600

    ts variable

---
+++ output/script.js
@@ -0,0 +1,17 @@
+"use strict";
+/*
+const country = `Bangladesh`;
+console.log(country);
+*/
+/*
+let playerName = `Masrafi`;
+let age = 22
+console.log(playerName);
+
+playerName = 34;
+console.log(playerName);
+*/
+let playerName;
+playerName = `Masrafi`;
+playerName = 34;
+console.log(playerName);
---
+++ src/script.ts
@@ -0,0 +1,20 @@
+/*
+const country = `Bangladesh`;
+console.log(country);
+*/
+
+/*
+let playerName = `Masrafi`;
+let age = 22
+console.log(playerName);
+
+playerName = 34;
+console.log(playerName);
+*/
+
+let playerName;
+
+playerName = `Masrafi`;
+playerName = 34;
+
+console.log(playerName);
