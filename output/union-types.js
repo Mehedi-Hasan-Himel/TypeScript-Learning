46a9d65ab8f830fd7755a75f78ff914b2957cff4
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-07-25 23:38:15 +0600

    union type data

---
+++ output/union-types.js
@@ -0,0 +1,11 @@
+"use strict";
+let myId;
+myId = `111`;
+myId = 111;
+myId = true;
+// console.log(myId);
+function displayUserInfo(userId) {
+    console.log(userId);
+}
+displayUserInfo(`101`);
+displayUserInfo(101);
---
+++ src/union-types.ts
@@ -0,0 +1,12 @@
+let myId: string | number | boolean;
+myId = `111`;
+myId = 111;
+myId = true;
+
+// console.log(myId);
+
+function displayUserInfo(userId: string | number) {
+  console.log(userId);
+}
+displayUserInfo(`101`);
+displayUserInfo(101);
