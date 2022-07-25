4b1bac0e01a8912e7db903a3ade569813663478f
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-07-26 00:18:34 +0600

    mix data

---
+++ output/touple-type.js
@@ -0,0 +1,8 @@
+"use strict";
+// mixed data - key value
+let user;
+user = [143, `himel`];
+console.log(user);
+user.push(43, `kuhok`);
+console.log(user);
+console.log(user[0]);
---
+++ src/touple-type.ts
@@ -0,0 +1,9 @@
+// mixed data - key value
+
+let user: [number, string];
+user = [143, `himel`];
+console.log(user);
+
+user.push(43, `kuhok`);
+console.log(user);
+console.log(user[0]);
