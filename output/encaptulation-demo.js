402ccb7ac89e7b477a7bba9259d3bcde47dc7eae
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-07-27 13:44:23 +0600

    encaptultaton

---
+++ output/encaptulation-demo.js
@@ -0,0 +1,25 @@
+"use strict";
+// public, private, protected, readonly.
+class User2 {
+    constructor(myName, age) {
+        this.myName = myName;
+        this.age = age;
+    }
+    display() {
+        console.log(`myName: ${this.myName}, age: ${this.age},id:${this.studentId}`);
+    }
+}
+class Students1 extends User2 {
+    constructor(myName, age, studentId) {
+        super(myName, age);
+        this.studentId = studentId;
+    }
+    display() {
+        console.log(`myName: ${this.myName}, age: ${this.age},id:${this.studentId}`);
+    }
+}
+let student3 = new Students1(`Himel`, 22, 143);
+// student3.display();
+let user6 = new User2(`Himel`, 22);
+user6.myName = `kuhok `;
+user6.display() = `kuhok `;
---
+++ src/encaptulation-demo.ts
@@ -0,0 +1,35 @@
+// public, private, protected, readonly.
+class User2 {
+  // properties, methods, constructor
+  public myName: string;
+  public age: number;
+  constructor(myName: string, age: number) {
+    this.myName = myName;
+    this.age = age;
+  }
+  display(): void {
+    console.log(
+      `myName: ${this.myName}, age: ${this.age},id:${this.studentId}`
+    );
+  }
+}
+
+class Students1 extends User2 {
+  studentId: number;
+  constructor(myName: string, age: number, studentId: number) {
+    super(myName, age);
+    this.studentId = studentId;
+  }
+  display(): void {
+    console.log(
+      `myName: ${this.myName}, age: ${this.age},id:${this.studentId}`
+    );
+  }
+}
+
+let student3 = new Students1(`Himel`, 22, 143);
+// student3.display();
+
+let user6 = new User2(`Himel`, 22);
+user6.myName = `kuhok `;
+user6.display() = `kuhok `;
