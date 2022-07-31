9a0feddaff065d798771ca59afe8cdcf0cc31537
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2022-07-31 17:13:34 +0600

    code

---
+++ output/interface.js
@@ -0,0 +1,9 @@
+"use strict";
+class UserC {
+    constructor(fullName, age) {
+        this.fullName = fullName;
+        this.age = age;
+    }
+}
+let userc = new UserC(`Mr. Potato`, 22);
+console.log(userc);
