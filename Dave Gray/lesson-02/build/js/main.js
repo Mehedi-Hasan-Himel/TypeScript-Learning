c147b4e294c9542098c8ebe969c2c442c4ee6bee
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-05-28 10:55:26 +0600

    [ADD] Typescript Basic Types

---
+++ Dave Gray/lesson-02/build/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+let myName = `Dave`;
+let meaningOfLife;
+let isLoading;
+let album;
+myName = `Jhon`;
+meaningOfLife = 42;
+isLoading = true;
+album = 1984;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let activeId;
+let re = /\w+/g;
---
+++ Dave Gray/lesson-02/src/main.ts
@@ -0,0 +1,19 @@
+let myName: string = `Dave`;
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
+myName = `Jhon`;
+meaningOfLife = 42;
+isLoading = true;
+album = 1984;
+
+
+const sum = (a:number, b:string) => {
+   return a + b;
+}
+
+
+let postId: string | number;
+let activeId: string | number;
+let re: RegExp = /\w+/g;
