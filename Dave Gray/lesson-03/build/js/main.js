87f0d501b64c9b9485e3ce9a9a43e675590ccf8b
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-05-28 16:41:18 +0600

    [ADD] type vs interface & object

--- Dave Gray/lesson-03/build/js/main.js
+++ Dave Gray/lesson-03/build/js/main.js
@@ -1,4 +1,5 @@
 "use strict";
+// Array Start
 let stringArr = [`one`, `hey`, `dave`];
 let guitars = [`Strat`, `Les Paul`, 5150];
 let mixData = [`EVH`, 1984, true];
@@ -9,3 +10,37 @@ guitars.unshift(`jim`);
 let test = [];
 let bands = [];
 bands.push(`Van Halen`);
+// Array End
+// Tuple Start
+let myTuple = [`Dave`, 1984, true];
+let mixed = [`Jhon`, 1, true];
+// myTuple[0] = 42;
+// Tuple End
+// Objext Start
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+myObj = bands;
+myObj = {};
+const exampleObj = {
+    prop1: `dave`,
+    prop2: true,
+};
+exampleObj.prop1 = `jhon`;
+let evh = {
+    name: `Eddie`,
+    active: true,
+    albums: [1, 2, 3, `ok bye`],
+};
+let jp = {
+    name: `Jimmy`,
+    active: true,
+    albums: [`I`, `II`, `III`],
+};
+const greetGuitarist = (guitarist) => {
+    if (guitarist.name) {
+        return `Hello ${guitarist.name.toUpperCase()}`;
+    }
+    return `Hello !`;
+};
+console.log(greetGuitarist(jp));
--- Dave Gray/lesson-03/src/main.ts
+++ Dave Gray/lesson-03/src/main.ts
@@ -1,18 +1,70 @@
+// Array Start
 let stringArr = [`one`, `hey`, `dave`];
 
 let guitars = [`Strat`, `Les Paul`, 5150];
 
 let mixData = [`EVH`, 1984, true];
 
-
 stringArr[0] = `jhon`;
 stringArr.push(`hey`);
 
 guitars[0] = 1984;
-guitars.unshift(`jim`)
+guitars.unshift(`jim`);
 
+let test = [];
 
-let test = []
+let bands: string[] = [];
+bands.push(`Van Halen`);
 
-let bands: string[] = []
-bands.push(`Van Halen`)
+// Array End
+
+// Tuple Start
+let myTuple: [string, number, boolean] = [`Dave`, 1984, true];
+
+let mixed = [`Jhon`, 1, true];
+
+// myTuple[0] = 42;
+
+// Tuple End
+
+// Objext Start
+let myObj: object;
+myObj = [];
+console.log(typeof myObj);
+
+myObj = bands;
+myObj = {};
+
+const exampleObj = {
+  prop1: `dave`,
+  prop2: true,
+};
+
+exampleObj.prop1 = `jhon`;
+
+interface Guitarist {
+  name?: string;
+  active: boolean;
+  albums: (string | number)[];
+}
+
+let evh: Guitarist = {
+  name: `Eddie`,
+  active: true,
+  albums: [1, 2, 3, `ok bye`],
+};
+
+let jp: Guitarist = {
+  name: `Jimmy`,
+  active: true,
+  albums: [`I`, `II`, `III`],
+};
+
+const greetGuitarist = (guitarist: Guitarist) => {
+  if (guitarist.name) {
+    return `Hello ${guitarist.name.toUpperCase()}`;
+  }
+   return `Hello !`;
+};
+
+console.log(greetGuitarist(jp));
