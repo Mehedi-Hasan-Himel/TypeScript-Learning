4baf0032f788ef715e57421817e672c91497c960
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-05-28 11:18:29 +0600

    [ADD]-[lesson 03 - Array]

---
+++ Dave Gray/lesson-03/build/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+let stringArr = [`one`, `hey`, `dave`];
+let guitars = [`Strat`, `Les Paul`, 5150];
+let mixData = [`EVH`, 1984, true];
+stringArr[0] = `jhon`;
+stringArr.push(`hey`);
+guitars[0] = 1984;
+guitars.unshift(`jim`);
+let test = [];
+let bands = [];
+bands.push(`Van Halen`);
---
+++ Dave Gray/lesson-03/src/main.ts
@@ -0,0 +1,18 @@
+let stringArr = [`one`, `hey`, `dave`];
+
+let guitars = [`Strat`, `Les Paul`, 5150];
+
+let mixData = [`EVH`, 1984, true];
+
+
+stringArr[0] = `jhon`;
+stringArr.push(`hey`);
+
+guitars[0] = 1984;
+guitars.unshift(`jim`)
+
+
+let test = []
+
+let bands: string[] = []
+bands.push(`Van Halen`)
