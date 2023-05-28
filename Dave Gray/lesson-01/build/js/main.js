8991a7d9488e70f293074de7e25b9ee978d8501f
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-05-28 10:25:33 +0600

    Dave Gray - Lesson 01

---
+++ Dave Gray/lesson-01/build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let userName = `Mehedi Hasan Himel`;
+console.log(userName);
+let a = 12;
+let b = 6;
+let c = 2;
+console.log(a / b);
+console.log(c * b);
---
+++ Dave Gray/lesson-01/src/main.ts
@@ -0,0 +1,10 @@
+let userName = `Mehedi Hasan Himel`;
+console.log(userName);
+
+let a: number = 12;
+let b: number = 6;
+let c: number = 2;
+
+console.log(a / b);
+
+console.log(c * b);
